83152de4fe1d23577efea75aeee2ce6e5a8f395f
josh <jlee12@ohiochristian.edu>
2025-04-13 19:48:31 -0400

    Fix date_helper module

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,6 @@ module WeatherApp
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.eager_load_paths << Rails.root.join("lib")
   end
 end
--- lib/utlis/date_helper.rb
+++ lib/utlis/date_helper.rb
@@ -1,20 +1,23 @@
-module DateHelper
-  class << self
-    def month_day(timestamp)
-      date = Time.at(timestamp).to_date
-      formatted_date = date.strftime('%b %d')
-      day_with_suffix(formatted_date, date.day)
+module Utils
+  module DateHelper
+    class << self
+      def month_day(timestamp)
+        date = Time.at(timestamp).to_date
+        formatted_date = date.strftime('%b %d')
+        day_with_suffix(formatted_date, date.day)
+      end
     end
-  end
 
-  def day_with_suffix(formatted_date, day)
-    suffix = case day
-              when 1, 21, 31 then 'st'
-              when 2, 22 then 'nd'
-              when 3, 23 then 'rd'
-              else 'th'
-              end
-    formatted_date.sub(/\d{2}/, "#{day}#{suffix}")
+    def day_with_suffix(formatted_date, day)
+      suffix = case day
+                when 1, 21, 31 then 'st'
+                when 2, 22 then 'nd'
+                when 3, 23 then 'rd'
+                else 'th'
+                end
+      formatted_date.sub(/\d{2}/, "#{day}#{suffix}")
+    end
+
   end
 
 end
